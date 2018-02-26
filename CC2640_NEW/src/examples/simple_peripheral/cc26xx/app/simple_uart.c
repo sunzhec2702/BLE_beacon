514d34d3645097221b7c83362c7a8405d4b003c1
sunzhec2702 <sunzhe_c2702@163.com>
2018-02-26 19:45:34 +0800

    OFFICE SAVE

--- CC2640_NEW/src/examples/simple_peripheral/cc26xx/app/simple_uart.c
+++ CC2640_NEW/src/examples/simple_peripheral/cc26xx/app/simple_uart.c
@@ -1,19 +1,22 @@
 #include "simple_uart.h"
+#include "simple_led.h"
 #include <string.h>
 #include <stdio.h>
+#include <ti/drivers/uart/UARTCC26XX.h>
 #include "Board.h"
-#include "npi_tl_uart.h"
 
-#ifdef NPI_USE_UART
+static UART_Handle uartHandle;
 
-
+/*
 static char tRxBuf[256];
 static char tTxBuf[256];
+*/
 
-static bool uartInitFlag = FALSE;
+static bool uartInitFlag = false;
 
 void UART_WriteTransport (uint8_t *str, uint16_t len)
 {
+    /*
     if(uartInitFlag)
     {
         memset(tTxBuf, 0, sizeof(tTxBuf));
@@ -21,11 +24,13 @@ void UART_WriteTransport (uint8_t *str, uint16_t len)
         memcpy(tTxBuf, str, len);
         NPITLUART_writeTransport(len);
     }
+    */
 }
 
-void  UartCallBack(uint16_t rxLen, uint16_t txLen)
+void UartReadCallBack(UART_Handle handle, void *buf, size_t count)
 {
-    if(uartInitFlag)
+    /*
+    if(uartInitFlag == true)
     {
     //   LCD_WRITE_STRING_VALUE("rxLen: ", (uint32_t)rxLen, 10, LCD_PAGE5);
     //   LCD_WRITE_STRING_VALUE("txLen: ", (uint32_t)txLen, 10, LCD_PAGE6);
@@ -39,21 +44,37 @@ void  UartCallBack(uint16_t rxLen, uint16_t txLen)
             NPITLUART_writeTransport(rxLen);
        }
     }
+    */
 }
 
 
-void Uart_Init()
+void uartInit()
 {
     if(!uartInitFlag)
     {
-        NPITLUART_initializeTransport(tRxBuf, tTxBuf, UartCallBack);
-        uartInitFlag = TRUE;
+        UART_Params params;
+        UART_Params_init(&params);
 
-        sprintf(tTxBuf, "NPITLUART_initialize");
-        NPITLUART_writeTransport(strlen(tTxBuf));
+        params.readMode = UART_MODE_CALLBACK;
+        params.writeMode = UART_MODE_CALLBACK;
+
+        params.readCallback = UartReadCallBack;
+        params.writeCallback = NULL;
+        params.readReturnMode = UART_RETURN_FULL;
+        params.readDataMode = UART_DATA_BINARY;
+        params.writeDataMode = UART_DATA_BINARY;
+        params.readEcho = UART_ECHO_OFF;
+        params.baudRate = 115200;
+        params.dataLength = UART_LEN_8;
+        params.stopBits = UART_STOP_ONE;
+        params.parityType = UART_PAR_NONE;
+
+        uartHandle = UART_open(Board_UART, &params);
+        if (!uartHandle)
+        {
+            ledBlinkWithParameters(LED_INDEX_0, 1000, 200, 10);
+        }
+        uartInitFlag = true;
+        UART_write(uartHandle, "HELLO\r\n", 7);
     }
 }
-
-#endif
-
-
