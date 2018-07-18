2c6c698772facb992ea5a5c63d0dc24434c6ecfc
sunzhec2702 <sunzhe_c2702@163.com>
2018-07-18 23:30:50 +0800

    vibra sensor seems done.

--- CC2640_NEW/src/examples/simple_peripheral/cc26xx/app/common/simple_vibra_sensor.c
+++ CC2640_NEW/src/examples/simple_peripheral/cc26xx/app/common/simple_vibra_sensor.c
@@ -13,8 +13,8 @@
 //static void Board_keyChangeHandler(UArg a0);
 static void vibraSensorCallback(PIN_Handle hPin, PIN_Id pinId)
 
-// Memory for the GPIO module to construct a Hwi
-Hwi_Struct callbackHwiKeys;
+    // Memory for the GPIO module to construct a Hwi
+    Hwi_Struct callbackHwiKeys;
 
 // PIN configuration structure to set all KEY pins as inputs with pullups enabled
 PIN_Config vibraIOCfg[] =
@@ -27,14 +27,26 @@ PIN_Handle hVibraSensorIO;
 static Clock_Struct vibraCheckClock;
 static bool vibraTriggered = false;
 
+void vibraIntControl(bool enable)
+{
+    if (enable)
+    {
+        PIN_setConfig(hVibraSensorIO, PIN_BM_IRQ, Board_Vibra_IO | PINCC26XX_IRQ_BOTHEDGES); // Both Edge
+        
+    }
+    else
+    {
+        PIN_setConfig(hVibraSensorIO, PIN_BM_IRQ, Board_Vibra_IO | PINCC26XX_IRQ_DIS); // Both Edge
+    }
+}
+
 void vibraSensorInit()
 {
     // Initialize vibra snesor pin. Enable int after callback registered
     hVibraSensorIO = PIN_open(&vibraSensorIO, vibraIOCfg);
     PIN_registerIntCb(hVibraSensorIO, vibraSensorCallback);
-    PIN_setConfig(hVibraSensorIO, PIN_BM_IRQ, Board_Vibra_IO | PINCC26XX_IRQ_BOTHEDGES); // Rising Edge
     PIN_setConfig(hVibraSensorIO, PINCC26XX_BM_WAKEUP, Board_Vibra_IO | PINCC26XX_WAKEUP_POSEDGE);
-
+    vibraIntControl(true);
     // Setup keycallback for keys
     Util_constructClock(&vibraCheckClock, vibraSensorCheckHandler,
                         VIBRA_SENSOR_CHECK_PERIOD, 0, false, 0);
@@ -43,12 +55,14 @@ void vibraSensorInit()
 static void vibraSensorCallback(PIN_Handle hPin, PIN_Id pinId)
 {
     vibraTriggered = true;
+    vibraIntControl(false);
     Util_startClock(&vibraCheckClock);
 }
 
 static void vibraSensorCheckHandler(UArg a0)
 {
     //TODO: reset the wake up time left and start the interrupt again.
+    vibraIntControl(true);
 }
 /*********************************************************************
 *********************************************************************/
